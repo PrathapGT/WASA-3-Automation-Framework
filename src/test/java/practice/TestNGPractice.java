5df17eb64ded53e36845a0f0e6aeb888c5e1c1fa
home <home@Prathapreddy>
2023-04-01 17:55:56 +0530

    Framework committed

---
+++ src/test/java/practice/TestNGPractice.java
@@ -0,0 +1,26 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class TestNGPractice {
+
+	
+	@Test
+	
+	public void sample() {
+		
+		System.out.println("prathap");
+	}
+	
+	
+	@Test
+	
+	public void sampletestscript() {
+		
+		System.out.println("Reddy");
+		
+		System.out.println("bangalore");
+	}
+	
+	
+}
